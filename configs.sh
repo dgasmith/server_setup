1f8317a5ebc6addc4924ff6125296f5080f22b76
Daniel G. A. Smith <dgasmith@icloud.com>
2019-09-18 14:16:10 -0400

    Basic configs and ZSH

---
+++ configs.sh
@@ -0,0 +1,2 @@
+ git config --global user.email "dgasmith@icloud.com"
+ git config --global user.name "Daniel G. A. Smith"
---
+++ install_zsh.sh
@@ -0,0 +1,8 @@
+sudo apt install zsh
+sudo apt-get install powerline fonts-powerline
+
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+
+sudo chsh -s /bin/zsh $USER
