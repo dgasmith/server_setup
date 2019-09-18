c271a1649189b50b96e867021cac0ce5423819e8
Daniel G. A. Smith <dgasmith@icloud.com>
2019-09-18 14:25:12 -0400

    Adds miniconda script

--- configs.sh
+++ configs.sh
@@ -1,2 +1,4 @@
- git config --global user.email "dgasmith@icloud.com"
- git config --global user.name "Daniel G. A. Smith"
+# GitHub configs 
+git config --global user.email "dgasmith@icloud.com"
+git config --global user.name "Daniel G. A. Smith"
+git config credential.helper cache
