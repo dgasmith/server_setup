f163f17874f9781e9dcafb2765fe82c28d1627cc
Daniel G. A. Smith <dgasmith@icloud.com>
2019-09-18 15:19:48 -0400

    Answers yes to zsh install

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,8 +1,9 @@
-sudo apt install zsh
-sudo apt-get install powerline fonts-powerline
+sudo -y apt install zsh
+sudo -y apt-get install powerline fonts-powerline
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
 
 sudo chsh -s /bin/zsh $USER
+/bin/zsh
