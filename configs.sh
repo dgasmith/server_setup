c9357cfc1a5b4442821ca02dc61be7d6e7956817
Daniel G. A. Smith <dgasmith@icloud.com>
2019-09-18 14:27:51 -0400

    Patchup

--- configs.sh
+++ configs.sh
@@ -1,4 +1,4 @@
 # GitHub configs 
 git config --global user.email "dgasmith@icloud.com"
 git config --global user.name "Daniel G. A. Smith"
-git config credential.helper cache
+git config --global credential.helper cache
